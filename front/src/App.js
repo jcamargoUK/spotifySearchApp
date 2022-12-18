833315ead8b6dc6fced88155de6bf7101a66a915
Julio Camargo <jcamargo@hotmail.co.uk>
2022-12-18 11:21:37 +0000

    comments added to the file

--- front/src/App.js
+++ front/src/App.js
@@ -1,78 +1,79 @@
-// import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Container, InputGroup, FormControl, Row, Card } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
+import './App.css'; // Import CSS
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import { Button, Container, InputGroup, FormControl, Row, Card } from 'react-bootstrap'; // Import Bootstrap Components
+import { useState, useEffect } from 'react'; // Import React Hooks
 
-const CLIENT_ID = 'b9c5fae6d8a14ae98dc71f7395057e6b';
-const CLIENT_SECRET = '58b476258832436e8bd7b1035b142506';
+const CLIENT_ID = 'b9c5fae6d8a14ae98dc71f7395057e6b'; // Your client id
+const CLIENT_SECRET = '58b476258832436e8bd7b1035b142506'; // Your secret
 
-function App() {
-  const [searchInput, setSearchInput] = useState('');
-  const [accessToken, setAccessToken] = useState('');
-  const [albums, setAlbums] = useState([]);
+function App() { 
+  const [searchInput, setSearchInput] = useState(''); // Search Input
+  const [accessToken, setAccessToken] = useState(''); // Access Token
+  const [albums, setAlbums] = useState([]); // Albums
 
   useEffect(() => {
     // API Access Token
     const authParameters = {
-      method: 'POST',
+      method: 'POST', 
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded' // Set content type to x-www-form-urlencoded
     },
-    body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET // Set body to grant_type, client_id, and client_secret
   }
-    fetch('http://accounts.spotify.com/api/token', authParameters)
-      .then(response => response.json())
-      .then(data => setAccessToken(data.access_token))
+    fetch('http://accounts.spotify.com/api/token', authParameters) // Get request to get access token
+       .then(response => response.json()) // Convert response to JSON
+      .then(data => setAccessToken(data.access_token)) // Set access token
       
   }, [])
 
   // Search
   async function search() {
-    console.log('searching for ' + searchInput);
+    console.log('searching for ' + searchInput); // Log search input to console
     // Get request using search to get Artist ID
     const artistParameters = {
       method: 'GET',
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + accessToken
+        'Content-Type': 'application/json', // Set content type to application/json
+        'Authorization': 'Bearer ' + accessToken // Set authorization to Bearer token
     }
   }
-    const artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', artistParameters)
-    .then(response => response.json())
-    .then(data => { return data.artists.items[0].id })
+    const artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', artistParameters) // Get request to get artist ID
+    .then(response => response.json()) // Convert response to JSON
+    .then(data => { return data.artists.items[0].id }) // Return artist ID
 
-    console.log("Artist ID is " + artistID);
+    console.log("Artist ID is " + artistID); // Log artist ID to console
     
-    // Get request using Artist ID to get Artist Albums
-
-    const returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=10', artistParameters)
-    .then(response => response.json())
-    .then(data => { console.log(data.items)
-    setAlbums(data.items) })
     
-    // Display Albums to the user
-    console.log(returnedAlbums);
+     const returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + 'include_groups=album&market=US&limit=10', artistParameters) // Get request to get albums
+    .then(response => response.json()) // Convert response to JSON
+    .then(data => { console.log(data.items) // Log albums to console
+    setAlbums(data.items) }) // Set albums to data.items
+    
+    
+    console.log(returnedAlbums); // Log returned albums to console
   }
   
   return (
-    <div className="App">
-     <Container>
-      <InputGroup className="mb-3" size='lg'>
+    // Return App and Bootstrap Components
+    <div className="App"> 
+     <Container> 
+      <InputGroup className="mb-3" size='lg'> 
         <FormControl
-          placeholder="Search for Artist"
+          placeholder="Search for Artist" // Placeholder
           type='input'
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              search()
+          onKeyPress={(e) => // On key press
+            {
+            if (e.key === 'Enter') // If enter is pressed
+            {
+              search() // Call search function
             }
           }}
           onChange={(e) => {
-            setSearchInput(e.target.value);
+            setSearchInput(e.target.value); // Set search input to value
           }}
         />
         <Button onClick={() => {
-          search()
+          search() // Call search function
         }}>Search</Button>
       </InputGroup>
      </Container>
@@ -80,17 +81,19 @@ function App() {
       <Row className='mx-2 row row-cols-4'>
         {albums.map((album, i) => {
           return(
-          <Card>
-            <Card.Img src={album.images[0].url} />
+            // The minimun card size is set to 300px
+          <Card style={{minWidth: 300}}> 
+          
+            <Card.Img src={album.images[0].url} /> 
             <Card.Body>
             <Card.Title>{album.name}</Card.Title>
-            </Card.Body>
-          </Card> 
+            </Card.Body> 
+          </Card> // Return Card
           ) 
         })}
          
       </Row>
-     </Container>
+     </Container> 
     </div>
   );
 }
